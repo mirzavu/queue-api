3a42f16d02be4037c30b0c86322bf4e530de4a96
Mirza V U <mirza.ekm@gmail.com>
2017-09-28 19:13:11 +0530

    fix

--- database/migrations/2017_09_20_022922_create_qr_table.php
+++ database/migrations/2017_09_20_022922_create_qr_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateQueueTable extends Migration
+class CreateQrTable extends Migration
 {
     /**
      * Run the migrations.
